92dab5b50e974cfd8169e7ce2519f0d90ba0e018
Yannick Marcon <yannick.marcon@obiba.org>
2021-09-17 14:56:11 +0200

    nginx config added

--- Dockerfile
+++ Dockerfile
@@ -17,6 +17,7 @@ RUN quasar build
 
 # production stage
 FROM nginx:alpine as production-stage
+COPY ./nginx/default.conf /etc/nginx/conf.d/default.conf
 COPY --from=build-stage /app/dist/spa /usr/share/nginx/html
 EXPOSE 80
 CMD ["nginx", "-g", "daemon off;"]
--- quasar.conf.js
+++ quasar.conf.js
@@ -48,7 +48,7 @@ module.exports = configure(function (ctx) {
 
     // Full list of options: https://quasar.dev/quasar-cli/quasar-conf-js#Property%3A-build
     build: {
-      vueRouterMode: 'hash', // available values: 'hash', 'history'
+      vueRouterMode: 'history', // available values: 'hash', 'history'
 
       env: {
         API: ctx.dev
