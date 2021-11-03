cf5e101abbbd24cd4c462fa44c2c37db250268b8
Yannick Marcon <yannick.marcon@obiba.org>
2021-11-03 18:39:17 +0100

    form items tree actions

--- src/components/forms/FormElement.vue
+++ src/components/forms/FormElement.vue
@@ -6,14 +6,15 @@
       <template v-slot:before>
         <div>
           <q-tree 
+            v-if="items.length>0"
             :nodes="items"
             node-key="name"
             children-key="items"
             default-expand-all
             selected-color="primary"
             v-model:selected="selected"
-            @keyup.alt.up="moveUp"
-            @keyup.alt.down="moveDown">
+            @keyup.alt.up="selectUpItem"
+            @keyup.alt.down="selectDownItem">
 
             <template v-slot:default-header="prop">
               <div class="row items-center">
@@ -112,7 +113,7 @@ export default defineComponent({
         this.form.items = [];
       }
       if (!this.formItemSelected && this.form.items.length>0) {
-        this.formItemSelected = this.form.items[0];
+        this.selected = this.form.items[0].name;
       }
       return this.formItemSelected;
     },
@@ -124,9 +125,12 @@ export default defineComponent({
   },
   watch: {
     selected: function(newValue, oldValue) {
-      console.log(newValue);
-      if (newValue !== null)
-        this.selectItem(this.form.items.filter(item => item.name === newValue).pop());
+      if (newValue === null) {
+        this.selected = oldValue;
+      } else {
+        const found = this.findItemAndParent(newValue);
+        this.selectItem(found.item);
+      }
     }
   },
   methods: {
@@ -134,48 +138,173 @@ export default defineComponent({
       this.formItemSelected = item;
     },
     addItem() {
-      this.form.items.push({ name: "item" + (this.form.items.length + 1) });
+      const found = this.findItemAndParent(this.selected);
+      let i = 1;
+      let test = this.findItemAndParent('ITEM' + i);
+      while (test.item !== null) {
+        i = i + 1;
+        test = this.findItemAndParent('ITEM' + i);
+      }
+      const newItem = { 
+        name: 'ITEM' + i,
+        type: 'static'
+      };
+      if (found.item !== null && found.item.type === 'group') {
+        // add last in the group
+        if (!found.item.items)
+          found.item.items = [newItem];
+        else
+          found.item.items.push(newItem);
+      } else {
+        // add after selected one
+        const idx = found.parent.items.indexOf(found.item);
+        found.parent.items.splice(idx+1, 0, newItem);
+      }
+      this.selected = newItem.name;
     },
-    moveUp() {
-      this.moveUpItem(this.formItemSelected);
-    },
-    moveUpItem(item) {
-      const idx = this.form.items.indexOf(item);
-      if (idx>0) {
-        this.form.items.splice(idx, 1);
-        this.form.items.splice(idx-1, 0, item);
+    selectUpItem() {
+      if (this.selected === null)
+        return;
+      
+      const found = this.findItemAndParent(this.selected);
+      const idx = found.parent.items.indexOf(found.item);
+      if (idx === 0 && found.parent.type === 'group')
+        this.selected = found.parent.name; // case selected item is a group
+      else if (idx>0) {
+        const upItem = found.parent.items[idx-1];
+        if (upItem.type === 'group' && upItem.items && upItem.items.length>0)
+          this.selected = upItem.items[upItem.items.length-1].name; // case selected item is after a group with items
+        else
+          this.selected = upItem.name; // case regular up item
       }
     },
-    moveDown() {
-      this.moveDownItem(this.formItemSelected);
+    moveUpItem(item) {
+      let found = this.findItemAndParent(item.name);
+      if (found.item === null)
+        return;
+      
+      let idx = found.parent.items.indexOf(item);
+      if (found.parent.type === 'group') {
+        found.parent.items.splice(idx, 1);
+        if (idx === 0) {
+          // case first in a group, move item before the group
+          found = this.findItemAndParent(found.parent.name);
+          idx = found.parent.items.indexOf(found.item);
+          found.parent.items.splice(idx, 0, item);
+        } else {
+          // case move up in the group
+          found.parent.items.splice(idx-1, 0, item);
+        }
+      } else if (idx>0) {
+        found.parent.items.splice(idx, 1);
+        const upItem = found.parent.items[idx-1];
+        if (upItem.type === 'group') {
+          // case up item is a group, then insert it as last item
+          if (!upItem.items)
+            upItem.items = [item];
+          else
+            upItem.items.push(item)
+        } else {
+          // regular case
+          found.parent.items.splice(idx-1, 0, item);
+        }
+      }
+    },
+    selectDownItem() {
+      if (this.selected === null)
+        return;
+
+      let found = this.findItemAndParent(this.selected);
+      if (found.item.type === 'group' && found.item.items && found.item.items.length>0)
+        this.selected = found.item.items[0].name; // case selected item is a group with items
+      else {
+        let idx = found.parent.items.indexOf(found.item);
+        if (idx === found.parent.items.length-1 && found.parent.type === 'group') {
+          found = this.findItemAndParent(found.parent.name);
+          idx = found.parent.items.indexOf(found.item); // case selected item is last item in a group
+        }
+        if (idx<found.parent.items.length-1)
+          this.selected = found.parent.items[idx+1].name; // case selected is last in the group
+      }
     },
     moveDownItem(item) {
-      const idx = this.form.items.indexOf(item);
-      if (idx<this.form.items.length) {
-        this.form.items.splice(idx, 1);
-        this.form.items.splice(idx+1, 0, item);
+      let found = this.findItemAndParent(item.name);
+      if (found.item === null)
+        return;
+
+      let idx = found.parent.items.indexOf(item);
+      if (found.parent.type === 'group') {
+        found.parent.items.splice(idx, 1);
+        if (idx === found.parent.items.length) {
+          // case it was last, move it after the parent
+          found = this.findItemAndParent(found.parent.name);
+          idx = found.parent.items.indexOf(found.item);
+        }
+        found.parent.items.splice(idx+1, 0, item);
+      } else if (idx<found.parent.items.length-1) {
+        found.parent.items.splice(idx, 1);
+        const downItem = found.parent.items[idx];
+        if (downItem.type === 'group') {
+          if (!downItem.items)
+            downItem.items = [item];
+          else
+            downItem.items.splice(0, 0, item);
+        } else {
+          // regular case
+          found.parent.items.splice(idx+1, 0, item);
+        }
       }
     },
     deleteItem(item) {
-      if (this.form.items) {
-        if (item === this.formItemSelected) {
-          let idx = this.form.items.indexOf(item);
-          console.log(idx)
-          console.log(this.formItemSelected)
-          if (idx>=1) {
-            this.selectItem(this.form.items[idx - 1]);
-          } else if (this.form.items.length>(idx + 1)) {
-            this.selectItem(this.form.items[idx + 1]);
-          } else {
-            this.selectItem(undefined);
-          }
-          console.log(this.formItemSelected)
-        }
-        this.form.items = this.form.items.filter(it => it.name !== item.name);
+      let found = this.findItemAndParent(item.name);
+      if (found.item === null)
+        return;
+
+      const idx = found.parent.items.indexOf(item);
+      if (idx === 0)
+        if (found.parent.type === 'group')
+          this.selected = found.parent.name;
+        else
+          this.selectDownItem(item);
+      else
+        this.selectUpItem(item);
+      found.parent.items.splice(idx, 1);
+      if (this.form.items.length === 0) {
+        this.selectItem(null);
       }
     },
     isItemSelected(item) {
       return this.formItemSelected && this.formItemSelected.name === item.name;
+    },
+    findItemAndParent(name) {
+      return this.findItemInParent(this.form, name);
+    },
+    findItemInParent(parent, name) {
+      let rval = {
+        parent: parent,
+        name: name,
+        item: null
+      };
+
+      if (parent.items) {
+        const item = parent.items.filter(it => it.name === name).pop();
+        if (item) {
+          rval.item = item;
+          return rval;
+        } else {
+          const groups = parent.items.filter(it => it.type === 'group');
+          if (groups) {
+            groups.forEach(group => {
+              if (rval.item === null) {
+                const grval = this.findItemInParent(group, name);
+                if (grval.item !== null)
+                  rval = grval;
+              }
+            });
+          }
+        }
+      }
+      return rval;
     }
   }
 })
