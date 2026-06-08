97339a89c3c02972505c605a736649de3df6d9d0
Yannick Marcon <yannick.marcon@obiba.org>
2026-06-08 10:57:35 +0200

    fix: remove last Vuex import from App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,22 +2,20 @@
   <router-view />
 </template>
 <script setup>
-import { computed, watch } from 'vue'
+import { watch } from 'vue'
 import { useI18n } from 'vue-i18n'
 import { useRouter } from 'vue-router'
-import { useStore } from 'vuex'
+import { useAuthStore } from 'src/stores/auth'
 
 const router = useRouter()
-const store = useStore()
+const authStore = useAuthStore()
 const { locale } = useI18n({ useScope: 'global' })
 
-const currentUser = computed(() => store.state.auth.user)
-
-watch(currentUser, (newUser) => {
+watch(() => authStore.user, (newUser) => {
   if (newUser === null) {
     router.push('/login')
-  } else {
-    locale.value = store.state.auth.payload.user.language
+  } else if (newUser?.language) {
+    locale.value = newUser.language
   }
 })
 </script>
